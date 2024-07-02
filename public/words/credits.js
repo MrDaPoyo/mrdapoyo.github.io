808fccb0abcdfbe9ab7cf2a0bb1cae112708a0df
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-07-02 20:48:16 +0200

    Delete public directory

--- public/words/credits.js
+++
@@ -1,2 +0,0 @@
-console.log('Hey...');
-console.log("Word effect(Javascript) by https://codepen.io/FlyC, All the rest including border, css, adjustments, the floating text and the insides by me(Poyo!)")
