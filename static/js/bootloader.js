e42c7cd944c173b42ccc924df9f6745b03c977a1
mrdapoyo <wiiuforever25@gmail.com>
2024-08-16 15:11:42 +0200

    hella cool animation

--- static/js/bootloader.js
+++ static/js/bootloader.js
@@ -1,3 +1,10 @@
-setTimeout(function() {
+var body = document.getElementsByTagName('body')[0];
+
+function loadDesktop() {
+    setTimeout(function() {
+    for (var i = 0; i < 100; i++) {
+        body.style.opacity = 1 - i / 100;
+    }
     window.location.href = '/desktop.html';
 }, 1000);
+}
