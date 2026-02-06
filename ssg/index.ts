7b84351e317e39c063c32e11cc60bee664d07f13
MrDaPoyo <wiiuforever25@gmail.com>
2026-02-06 22:19:48 +0100

    clear dir

--- ssg/index.ts
+++ ssg/index.ts
@@ -1,6 +1,18 @@
 import path from "node:path";
 import { utils } from "./utils.ts";
-import { readdir } from "node:fs/promises";
+import { readdir, rm } from "node:fs/promises";
+import { parseArgs } from "util";
+
+const { values, positionals } = parseArgs({
+    args: Bun.argv,
+    options: {
+        clr_out: {
+            type: "boolean",
+        },
+    },
+    strict: true,
+    allowPositionals: true,
+});
 
 if (!process.env.SOURCE_DIR) {
     console.error("Missing SOURCE_DIR");
@@ -8,7 +20,7 @@ if (!process.env.SOURCE_DIR) {
 }
 
 const source_dir = utils.findRootDir();
-const output_dir = path.join("..", process.env.OUTPUT_DIR);
+const output_dir = path.join("..", Bun.env.OUTPUT_DIR);
 
 if (!output_dir) {
     console.error("Missing OUTPUT_DIR");
@@ -17,6 +29,12 @@ if (!output_dir) {
 
 await utils.ensureDir(source_dir);
 await utils.ensureDir(output_dir);
+
+if (values.clr_out) {
+    await rm(output_dir, { recursive: true });
+    await utils.ensureDir(output_dir);
+    console.log("Wiped output dir.")
+}
 
 const files = await readdir(source_dir, { recursive: true });
 
